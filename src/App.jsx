e4387328db6e4b5b51b7ea1c9b5993a84b0d9f41
Xinran Wang <126731907+xinranwangxwan559@users.noreply.github.com>
2024-01-26 00:57:32 +0000

    xinran wang portfoilo

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Xinran Wang",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "xinran.wang@studentambassadors.com.com",
+  gitHub: "xinranwangxwan559",
+  instagram: "swppethechris",
+  linkedIn: "www.linkedin.com/in/xinran-wang-591627257",
   medium: "",
   twitter: "microsoft",
-  youTube: "Code",
+  youTube: "microsoft",
 };
 
 const primaryColor = "#4E567E";
